f30a644bed347a9c60f0bc94ee993a6d0dc96e40
rogpe <prog_x@hotmail.com>
2019-10-12 09:40:56 -0300

    Updates

---
+++ precache-manifest.c88eccc1910229d8d3e3dd97e4387e75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe934f5c24d87ac888b90d98bb75c874",
+    "url": "/hello/index.html"
+  },
+  {
+    "revision": "4896a2ea479ed98eadbb",
+    "url": "/hello/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "48bb2e603e4b312d12fb",
+    "url": "/hello/static/css/main.d6c60f13.chunk.css"
+  },
+  {
+    "revision": "4896a2ea479ed98eadbb",
+    "url": "/hello/static/js/2.3758c3da.chunk.js"
+  },
+  {
+    "revision": "48bb2e603e4b312d12fb",
+    "url": "/hello/static/js/main.656d6d45.chunk.js"
+  },
+  {
+    "revision": "3767d49c1029f0aba4bc",
+    "url": "/hello/static/js/runtime-main.9c2d849a.js"
+  }
+]);
